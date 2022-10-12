721f2474a82e9a87203fcb5b0c5a9023f30b0ade
Алекс <chu@mail.ru>
2022-10-12 15:25:19 +0300

    задания к уроку 8

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,36 @@
 public class Main {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         int salary = 15000;
         int total = 0;
         int months = 0;
-        while (total< 2_459_000){
+        while (total < 2_459_000) {
             total = total + salary;
-            months = months +1;
-            System.out.println("Понадобится " + months + " месяцев, что бы накопить " + total + " рублей");}
-
+            months = months + 1;
+            System.out.println("Понадобится " + months + " месяцев, что бы накопить " + total + " рублей");
+        }
 
 
         int f = 0;
-        while (f<10){
+        while (f < 10) {
             f++;
-            System.out.print(f +" " );
+            System.out.print(f + " ");
         }
         System.out.println();
-        for(int i=10; i>0; i-- ){
-            System.out.print(i+ " ");
+        for (int i = 10; i > 0; i--) {
+            System.out.print(i + " ");
         }
 
         System.out.println();
 
         int population = 12_000_000;
-        int populationKf = population/1000;
-        int fertility = populationKf*17;
-        int mortality = populationKf*8;
-        int totalGrowthPerYear = fertility-mortality;
+        int populationKf = population / 1000;
+        int fertility = populationKf * 17;
+        int mortality = populationKf * 8;
+        int totalGrowthPerYear = fertility - mortality;
         int totalPopulation = 0;
-        for (int year = 1; year <=10; year++){
-            totalPopulation = totalPopulation+population+totalGrowthPerYear;
-            System.out.println("год " + year + " численгость "+ totalPopulation);
+        for (int year = 1; year <= 10; year++) {
+            totalPopulation = totalPopulation + population + totalGrowthPerYear;
+            System.out.println("год " + year + " численность " + totalPopulation);
         }
 
 
@@ -38,36 +38,34 @@ public class Main {
 
         int salary1 = 15_000;
         int totalCash = 0;
-        for (int i=0 ; totalCash<=12_000_000; i++){
-            totalCash = totalCash + totalCash*7/100;
+        for (int i = 0; totalCash <= 12_000_000; i++) {
+            totalCash = totalCash + totalCash * 7 / 100;
             totalCash = totalCash + salary1;
-            if ( i % 6 == 0 )
-
-            System.out.println("месяц " + i + " ,денег на счете "+ totalCash);
+            if (i % 6 == 0){
+                System.out.println("месяц " + i + " ,денег на счете " + totalCash);}
 
 
         }
 
         System.out.println("задание 2.3");
 
-        int salary2= 15000;
+        int salary2 = 15000;
         int totalcash1 = 0;
-        int nineYears = 9*12;
-        for (int a=0; a<=nineYears; a++){
-            totalcash1 = totalcash1 + totalcash1*7/100;
+        int nineYears = 9 * 12;
+        for (int a = 0; a <= nineYears; a++) {
+            totalcash1 = totalcash1 + totalcash1 * 7 / 100;
             totalcash1 = totalcash1 + salary2;
-            if( a%6==0)
-                System.out.println("месяц " + a + " ,денег на счете "+ totalcash1);
+            if (a % 6 == 0)
+            {System.out.println("месяц " + a + " ,денег на счете " + totalcash1);}
 
         }
 
         System.out.println("задание 2.4");
 
 
+        for (int friday = 5; friday < 31; friday = friday + 7) {
 
-        for( int friday = 5; friday<31; friday=friday+7 ){
-
-                System.out.println("Сегодня пятница "+ friday + "-e число, нужно подготовить отчет");
+            System.out.println("Сегодня пятница " + friday + "-e число, нужно подготовить отчет");
         }
 
 
@@ -88,26 +86,18 @@ public class Main {
         int year = 2022;
         int lastYear = year - 201;
         int futureYear = year + 100;
-        for (int d = 0; d<futureYear; d=d+79 ){
-            if (d>lastYear && d<futureYear)
-            System.out.println(d);
+        for (int d = 0; d < futureYear; d = d + 79) {
+            if (d > lastYear && d < futureYear)
+                System.out.println(d);
         }
 
         System.out.println("задание 3.2");
         int two = 1;
         int num = 2;
 
-        for (int q=2; q<22; q=q+2){
-
-            System.out.println(two++ + "*" + num + " = " + q);}
-
-
-
-
-
-
-
-
+        for (int q = 2; q < 22; q = q + 2) {
+            System.out.println(two++ + "*" + num + " = " + q);
+        }
 
 
     }
